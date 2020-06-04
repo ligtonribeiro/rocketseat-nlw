1e50ea498b55cd46ae2924d48baded7daf9add6e
Wellington <ligton.ribeiro@gmail.com>
2020-06-04 00:03:46 -0300

    Criação do primeiro componente (Header)

---
+++ web/src/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>Ecoleta</h1>
+        </header>
+    );
+}
+
+// Iniciar a partir do minuto 23 do video.
+
+export default Header;
